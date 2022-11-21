54140aeace6bed9cf9e86f5230fb7b97b622f48f
acroscarrillo <49346947+acroscarrillo@users.noreply.github.com>
2022-11-21 23:54:14 +0000

    debounce_attiny84 code added

---
+++ useful_code_used/interrupt_attiny_84.ino
@@ -0,0 +1,27 @@
+#include <avr/interrupt.h>
+
+const byte ledPin = PCINT1;
+const byte interruptPin = INT0;
+volatile byte state = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(0, blink, FALLING);
+}
+
+void loop() {
+  digitalWrite(ledPin, state);
+}
+
+void blink() {
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time > 300) 
+  {
+   state=!state;
+  }
+  last_interrupt_time = interrupt_time;
+}}
